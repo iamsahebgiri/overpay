50425dd832d1e9a12a5275e57719fea043595f0c
Saheb Giri <iamsahebgiri@gmail.com>
2022-09-05 20:22:43 +0530

    chore: add functionality to carousel components

--- components/common/carousel.jsx
+++ components/common/carousel.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import { classNames } from "lib/classnames";
+import React, { useEffect, useState } from "react";
 import { Overpay } from "./logo";
 
 const Carousel = ({ hasLogo = false }) => {
+  const contents = [
+    {
+      id: 1,
+      image: "/images/carousel/get-better-with-money.png",
+      title: "Get better with money",
+      body: "Overpay help you set saving goals, earn cash back offers, Go to disclaimer for more details and get paychecks up to two days early. Get a <span class='text-yellow-400'>$20</span> bonus when you receive qualifying direct deposits",
+    },
+    {
+      id: 2,
+      image: "/images/carousel/speedy-easy-fast.png",
+      title: "Speedy, Easy and Fast",
+      body: "Overpay help you set saving goals, earn cash back offers, Go to disclaimer for more details and get paychecks up to two days early. Get a <span class='text-yellow-400'>$20</span> bonus when you receive qualifying direct deposits",
+    },
+    {
+      id: 3,
+      image: "/images/carousel/get-better-with-money.png",
+      title: "Built for the gen z",
+      body: "Overpay help you set saving goals, earn cash back offers, Go to disclaimer for more details and get paychecks up to two days early. Get a <span class='text-yellow-400'>$20</span> bonus when you receive qualifying direct deposits",
+    },
+  ];
+
+  const [current, setCurrent] = useState(1);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrent((current) => (current + 1) % contents.length);
+    }, 5000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
     <div className="p-4 h-full">
       <div className="bg-brand-500 h-full relative">
@@ -10,26 +44,39 @@ const Carousel = ({ hasLogo = false }) => {
             <Overpay type="white" />
           </div>
         )}
-        <div className="bg-pattern h-full bg-contain bg-center bg-no-repeat px-6 py-8 flex flex-col justify-center">
+        <div className="bg-pattern h-full bg-contain bg-center bg-no-repeat px-6 py-8 flex flex-col justify-between items-center">
+          <div />
           <div>
-            <div className="py-8">
+            <div className="py-3">
               <img
-                src="/images/carousel/get-better-with-money.png"
-                alt="Get better with money"
+                src={contents[current].image}
+                alt={contents[current].title}
                 className="mx-auto"
               />
             </div>
-            <h3 className="text-4xl text-center font-extrabold text-white">
-              Get better with money
-            </h3>
-            <div className="max-w-lg mx-auto">
-              <p className="mt-2 text-sm text-center text-slate-300">
-                Overpay help you set saving goals, earn cash back offers, Go to
-                disclaimer for more details and get paychecks up to two days
-                early. Get a $20 bonus when you receive qualifying direct
-                deposits
-              </p>
+            <div>
+              <h3 className="text-4xl text-center font-extrabold text-white">
+                {contents[current].title}
+              </h3>
+              <div className="max-w-lg mx-auto">
+                <p
+                  className="mt-2 text-sm text-center text-slate-300"
+                  dangerouslySetInnerHTML={{ __html: contents[current].body }}
+                />
+              </div>
             </div>
+          </div>
+          <div className="flex space-x-1 mt-3">
+            {contents.map((content, i) => (
+              <div
+                key={content.id}
+                className={classNames(
+                  "h-2 rounded-full hover:cursor-pointer",
+                  current === i ? "w-8 bg-white" : "w-2 bg-slate-300"
+                )}
+                onClick={() => setCurrent(i)}
+              />
+            ))}
           </div>
         </div>
       </div>
