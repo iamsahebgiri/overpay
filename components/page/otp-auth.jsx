df398fd8a11c8e3393b54badbf7a974d64557c27
Saheb Giri <iamsahebgiri@gmail.com>
2022-09-05 11:07:40 +0530

    draft: verify otp auth page

---
+++ components/page/otp-auth.jsx
@@ -0,0 +1,47 @@
+import { Button } from "components/common/button";
+import { TextField } from "components/common/text-field";
+import FullHeader from "components/layout/full-header";
+import React from "react";
+
+const OtpAuth = () => {
+  return (
+    <FullHeader>
+      <div className="space-y-6">
+        <div>
+          <h2 className="text-3xl text-center font-extrabold text-slate-900">
+            Verify your email
+          </h2>
+          <p className="mt-3 text-sm text-center text-slate-500">
+            We have sent code to your email
+          </p>
+          <p className="text-sm font-medium text-center text-slate-800">
+            alesiaka******@mail.com
+          </p>
+        </div>
+        <div>
+          <form action="#" method="POST">
+            <div>
+              <TextField
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                placeholder="Username or email"
+                required
+              />
+            </div>
+          </form>
+        </div>
+        <div>
+          <Button className="w-full">Verify Account</Button>
+          <div className="w-full mt-4 text-center">
+            Resend code in{" "}
+            <span className="font-extrabold text-slate-700">59:00</span>
+          </div>
+        </div>
+      </div>
+    </FullHeader>
+  );
+};
+
+export default OtpAuth;
---
+++ pages/verify-otp.jsx
@@ -0,0 +1,12 @@
+import OtpAuth from "components/page/otp-auth";
+import React from "react";
+
+const VerifyOtpPage = () => {
+  return (
+    <div>
+      <OtpAuth />
+    </div>
+  );
+};
+
+export default VerifyOtpPage;
