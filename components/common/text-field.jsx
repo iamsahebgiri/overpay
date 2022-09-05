ab39c177c1620b78cefdb65da3911c275d4a4004
Saheb Giri <iamsahebgiri@gmail.com>
2022-09-05 19:11:33 +0530

    chore: add password field

--- components/common/text-field.jsx
+++ components/common/text-field.jsx
@@ -1,8 +1,11 @@
+import { FiEyeOff, FiEye } from "react-icons/fi";
 import { classNames } from "lib/classnames";
 import * as React from "react";
 
 export const TextField = React.forwardRef(
   ({ label, id, name, type = "text", className, ...rest }, forwardedRef) => {
+    const [show, setShow] = React.useState(false);
+
     return (
       <div>
         {label && (
@@ -10,17 +13,42 @@ export const TextField = React.forwardRef(
             {label}
           </label>
         )}
-        <input
-          {...rest}
-          ref={forwardedRef}
-          id={id || name}
-          name={name}
-          type={type}
-          className={classNames(
-            "block w-full py-3 rounded-xl placeholder-slate-400 bg-white border border-slate-300",
-            className
+        <div className="relative flex items-center">
+          <input
+            {...rest}
+            ref={forwardedRef}
+            id={id || name}
+            name={name}
+            type={show ? "text" : type}
+            className={classNames(
+              "block w-full py-3 rounded-xl placeholder-slate-400 bg-white border border-slate-300",
+              type === "password" && "pr-12",
+              className
+            )}
+          />
+          {type === "password" && (
+            <div className="absolute inset-y-0 right-0 pr-3 flex items-center">
+              <button
+                onClick={(e) => {
+                  e.preventDefault();
+                  setShow((show) => !show);
+                }}
+              >
+                {show ? (
+                  <FiEye
+                    className="h-5 w-5 text-slate-400"
+                    aria-hidden="true"
+                  />
+                ) : (
+                  <FiEyeOff
+                    className="h-5 w-5 text-slate-400"
+                    aria-hidden="true"
+                  />
+                )}
+              </button>
+            </div>
           )}
-        />
+        </div>
       </div>
     );
   }
